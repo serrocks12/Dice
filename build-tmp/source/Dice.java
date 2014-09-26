f8fd6b79f7b6cce9a760439a99c6f365a1c626ac
sebenchohra <sbenchohra12@gmail.com>
2014-09-26 11:12:15 -0700

    Added first name to index.html

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,12 +1,28 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die bob, jeff;
-void setup()
+public void setup()
 {
 	size(500, 500);
 	bob = new Die(140,200);
 	jeff = new Die(290,200);
 	noLoop();
 }
-void draw()
+public void draw()
 {
 	background(125,110,24);
 	jeff.roll();
@@ -19,7 +35,7 @@ void draw()
 
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -34,12 +50,12 @@ class Die      //models one single dice cube
 		size = 100;
 		
 	}
-	void roll()
+	public void roll()
 	{
 		numDots = (int)(Math.random()*6)+1;
 			
 	}
-	void show()
+	public void show()
 	{
 	 	
 	 	fill(255);
@@ -90,4 +106,13 @@ class Die      //models one single dice cube
 	}
 
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
